c15b15072144191131e5d692f3ca8aa71cf0116c
Kaue Oliveira <knascimento353@gmail.com>
2024-03-27 13:04:13 -0300

    coleta e salva os dados

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 """Importação do selenium para automatização"""
+import json
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+# from pathlib import Path
+
+# PATH_DATA = Path(__file__).parent / 'carro.json'
 
 navegador = webdriver.Edge()
 
@@ -12,14 +16,14 @@ navegador.maximize_window()
 
 # Ordem dos cliques -> copiar Xpath
 
-# Primeiro clicar em consulta e utílitários pequenos
+# 1º - clica em consulta e utílitários pequenos
 elemento = WebDriverWait(navegador, 10).until(EC.presence_of_element_located(
     (By.XPATH, '//*[@id="front"]/div[1]/div[2]/ul/li[1]/a/div[2]')
 )).click()
 
 
 sleep(2)
-# segundo clicar e selecionar o mes de pesquisa
+# 2º - clica e seleciona o mes de pesquisa
 navegador.find_element(
     By.XPATH, '//*[@id="selectTabelaReferenciacarro_chosen"]/a/div/b').click()
 
@@ -30,17 +34,17 @@ lista_mes_ano[1].click()
 
 
 sleep(2)
-# terceiro clicar e selecionar a marca
+# 3º - clica e seleciona a marca
 navegador.find_element(
     By.XPATH, '//*[@id="selectMarcacarro_chosen"]/a/div/b').click()
 opcoes_marca = navegador.find_elements(
     By.XPATH, '//*[@id="selectMarcacarro_chosen"]/div/ul')
 lista_marca = opcoes_marca[0].find_elements(By.CSS_SELECTOR, 'li')
-lista_marca[2].click()
+lista_marca[1].click()
 
 
 sleep(2)
-# quarto clicar e selecionar o modelo
+# 4º - clica e seleciona o modelo
 navegador.find_element(
     By.XPATH, '//*[@id="selectAnoModelocarro_chosen"]/a/div/b').click()
 opcoes_modelo = navegador.find_elements(
@@ -50,18 +54,34 @@ lista_modelo[2].click()
 
 
 sleep(2)
-# quito clicar e selecionar ano/modelo
+# 5º - clica e seleciona ano/modelo
 navegador.find_element(
     By.XPATH, '//*[@id="selectAnocarro_chosen"]/a/div/b').click()
 opcoes_ano_modelo = navegador.find_elements(
     By.XPATH, '//*[@id="selectAnocarro_chosen"]/div/ul')
 lista_ano_modelo = opcoes_ano_modelo[0].find_elements(By.CSS_SELECTOR, 'li')
-lista_ano_modelo[3].click()
+lista_ano_modelo[1].click()
 
 
-# sexto clicar e selecionar em pesquisar
+sleep(2)
+# 6º -  clica e seleciona botão pesquisar
 navegador.find_element(By.LINK_TEXT, 'Pesquisar').click()
 
+sleep(2)
+# 7º - cria dicionário onde salvaremos os dados
 
-# ultimo salvar os dados da tabela em dicionários
-sleep(10)
+# coleta os dados
+tabela = navegador.find_elements(
+    By.XPATH, '//*[@id="resultadoConsultacarroFiltros"]/table/tbody')
+linhas = tabela[0].find_elements(By.CSS_SELECTOR, 'td')
+
+# adiciona dados no dicionário
+carro = {
+    linhas[item].text: linhas[item + 1].text
+    for item in range(0, len(linhas)-1, 2)
+}
+
+
+object_json = json.dumps(carro, indent=2, ensure_ascii=False)
+with open('carro.json', 'w', encoding='utf-8') as file:
+    file.write(object_json)
